d0e4f646f21f83420a88527e32dd63f86d0a1497
valentinamadonia <valentinamadonia7@gmail.com>
2024-05-27 16:14:55 -0300

    Se agregaron metodos de actualizar, borrar y obtener en el controllador

--- src/main/java/spring/controller/NotaController.java
+++ src/main/java/spring/controller/NotaController.java
@@ -1,12 +1,18 @@
 package spring.controller;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import spring.entity.Nota;
+import spring.model.MNota;
 import spring.service.NotaService;
 
 @RestController
@@ -21,5 +27,20 @@ public class NotaController {
     @PutMapping("/nota")
     public boolean agregarNota(@RequestBody Nota nota){
         return service.crear(nota);
+    }
+    
+    @PostMapping("/nota")
+    public boolean actualizarNota(@RequestBody Nota nota){
+        return service.actualizar(nota);
+    }
+    
+    @DeleteMapping("/nota/{id}/{nombre}")
+    public boolean borrarNota(@PathVariable("id")long id, @PathVariable("nombre")String nombre){
+        return service.borrar(nombre, id);
+    }
+    
+    @GetMapping("/notas")
+    public List<MNota> obtenerNotas(){
+        return service.obtener();
     }
 }
